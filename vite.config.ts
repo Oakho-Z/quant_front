73130860b9e521e227b9e30fc7773bd68cb786ab
Oakho-Z <zhangtong0630@outlook.com>
2025-08-07 21:29:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,24 +21,24 @@ export default defineConfig(({ mode }) => {
       proxy: {
         // 代理所有 /run_pipeline 路径，保持路径不变，转发到后端
         '/run_pipeline': {
-          target: 'quant_backtend.railway.internal',
+          target: 'https://quantbacktend-production.up.railway.app',
           changeOrigin: true,
         },
         // 如果你还有其他接口需要代理，比如 /chart_data
         '/performance_tab_chart_data': {
-          target: 'quant_backtend.railway.internal',
+          target: 'https://quantbacktend-production.up.railway.app',
           changeOrigin: true,
         },
         '/performance_data': {
-          target: 'quant_backtend.railway.internal',
+          target: 'https://quantbacktend-production.up.railway.app',
           changeOrigin: true,
         },
         '/drawdown_data': {
-          target: 'quant_backtend.railway.internal',
+          target: 'https://quantbacktend-production.up.railway.app',
           changeOrigin: true,
         },
         '/weekly_market_mood_gauge_data': {
-          target: 'quant_backtend.railway.internal',
+          target: 'https://quantbacktend-production.up.railway.app',
           changeOrigin: true,
         },
       }
